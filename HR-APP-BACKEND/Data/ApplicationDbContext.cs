8a1c27d8de05964627d343f019179b6c92edbdf7
Mohammad-shehab <your.q8mlg@outlook.com>
2025-03-05 11:46:11 +0300

    Add project files.

---
+++ HR-APP-BACKEND/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HR_APP_BACKEND.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_APP_BACKEND.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+ 
+
+
+    }
+}
---
+++ HR-APP-BACKEND/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HR_APP_BACKEND.Models
+{
+    public class ApplicationUser : IdentityUser 
+    {
+    }
+}
